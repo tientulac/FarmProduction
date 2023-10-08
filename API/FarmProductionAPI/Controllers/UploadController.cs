2f4bcaab870d935dfd728e91c87c5ba4dd344bec
Nguyen Ngoc Tien <64886876+tientulac@users.noreply.github.com>
2023-10-08 21:36:36 +0700

    add upload

---
+++ API/FarmProductionAPI/Controllers/UploadController.cs
@@ -0,0 +1,61 @@
+﻿using FarmProductionAPI.Core.Commands.BrandCommand;
+using FarmProductionAPI.Core.Queries.BrandQuery;
+using FarmProductionAPI.Domain.Dtos;
+using FarmProductionAPI.Domain.Response;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FarmProductionAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Produces("application/json")]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ResponseResultAPI<bool>> UploadImage(IFormFile file, CancellationToken cancellationToken)
+        {
+            string path = "";
+            try
+            {
+                if (file.Length > 0)
+                {
+                    path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "UploadedFiles"));
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    using (var fileStream = new FileStream(Path.Combine(path, file.FileName), FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                    }
+                    return new ResponseResultAPI<bool>()
+                    {
+                        Code = "200",
+                        Data = true,
+                        Message = path + $"\\{file.FileName}"
+                    };
+                }
+                else
+                {
+                    return new ResponseResultAPI<bool>()
+                    {
+                        Code = "500",
+                        Data = false,
+                        Message = path
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ResponseResultAPI<bool>()
+                {
+                    Code = "500",
+                    Data = false,
+                    Message = ex.Message,
+                    MessageEX = ex.ToString()
+                };
+            }
+        }
+    }
+}
---
+++ Frontend/farm-ui/src/app/services/upload-image.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { AppConfig, AppConfiguration } from 'src/configuration';
+import { ReponseAPI } from '../entities/ResponseAPI';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UploadImageService {
+
+
+    constructor(
+        @Inject(AppConfig) private readonly appConfig: AppConfiguration,
+        private router: Router,
+        private http: HttpClient,
+    ) {
+    }
+
+    setHeader() {
+        return new HttpHeaders().set('Authorization', `Bearer ${''}`)
+    }
+
+    upload(formData: FormData): Observable<any> {
+        return this.http.post<any>(this.appConfig.API + 'Upload/UploadImage', formData, {
+            headers: this.setHeader(),
+        }).pipe(map((res) => { return res; }));
+    }
+}
+
