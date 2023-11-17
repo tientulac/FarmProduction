d213eae15ecd538566907f7c81a3b80e25226d57
Nguyen Ngoc Tien <tiennn26@GSHCM-TIENNN26>
2023-11-17 14:39:28 +0700

    c

--- API/FarmProductionAPI/Program.cs
+++ API/FarmProductionAPI/Program.cs
@@ -8,6 +8,7 @@ using FarmProductionAPI.Middlewares;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Serilog;
 using System.Text;
 
@@ -61,7 +62,35 @@ builder.Services.AddTransient<IExportExcel<ProductExport>, ExportToExcel<Product
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.InstallServicesInAssembly<IFarmProductionInfrastructureMarker>(configuration);
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Test01", Version = "v1" });
 
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme."
+
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                          {
+                              Reference = new OpenApiReference
+                              {
+                                  Type = ReferenceType.SecurityScheme,
+                                  Id = "Bearer"
+                              }
+                          },
+                         new string[] {}
+                    }
+                });
+});
 //JWT Authentication
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
